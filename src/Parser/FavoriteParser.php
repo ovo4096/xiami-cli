c1529ffb7bb42b3e0894c20dac065e559d8f5a67
owwo <owwo@outlook.com>
2017-03-06 12:15:59 +0800

    Add favorite parser

---
+++ src/Parser/FavoriteParser.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Xiami\Console\Parser;
+
+use Xiami\Console\Models\Song;
+use Xiami\Console\Models\Artist;
+
+class FavoriteParser
+{
+    public static function Songs($html)
+    {
+        $songMatches = [];
+        preg_match_all(
+            '/<tr\sdata-needpay="[\s\S]*?value="(?<id>\d*)"\s(?<inStock>checked|disabled)[\s\S]*?title="(?<name>.*?)"[\s\S]*?(?:(?<artists>(?:<a\sclass="artist_name"[\s\S]*?\/a>\s?)+)(?<=>)[\s\S]*?(?=<\/td>))[\s\S]*?value="(?<rate>\d)"[\s\S]*?\/tr>/',
+            $html,
+            $songMatches,
+            PREG_SET_ORDER
+        );
+
+        $songs = [];
+        foreach ($songMatches as $matche) {
+            $song = new Song();
+            $song->id = $matche['id'] + 0;
+            $song->name = html_entity_decode($matche['name'], ENT_QUOTES);
+            $song->inStock = $matche['inStock'] === 'checked';
+            $song->rate = $matche['rate'] + 0;
+
+            $artistMatches = [];
+            preg_match_all(
+                '/e="(?<name>.*?)"/',
+                $matche['artists'],
+                $artistMatches,
+                PREG_SET_ORDER
+            );
+
+            foreach ($artistMatches as $matche) {
+                $artist = new Artist();
+                $artist->name = $matche['name'];
+                $song->artists[] = $artist;
+            }
+
+            $songs[] = $song;
+        }
+
+        return $songs;
+    }
+}
--- xiami.php
+++ xiami.php
@@ -1,5 +1,8 @@
 #!/usr/bin/env php
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
