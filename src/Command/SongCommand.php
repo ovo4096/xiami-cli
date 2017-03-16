6319b374cbc982be8ab2fa0c6ffd095fefb21fe9
owwo <owwo@outlook.com>
2017-03-16 08:19:07 +0800

    Add helper

--- src/Command/SongCommand.php
+++ src/Command/SongCommand.php
@@ -6,12 +6,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\OutputStyle;
-use Symfony\Component\Console\Helper\ProgressBar;
-use GuzzleHttp\Client;
 use Xiami\Console\Style\AwesomeStyle;
 use Xiami\Console\Model\Song;
 use Xiami\Console\Exception\GetPlaylistJsonException;
-use Xiami\Console\Exception\DownloadSongException;
+use Xiami\Console\Helper\Helper;
 
 class SongCommand extends Command
 {
@@ -54,7 +52,7 @@ class SongCommand extends Command
                 $this->show($song, $io);
                 return;
             }
-            $this->download($song, $downloadPath, $downloadQuality, $io, $output);
+            Helper::download($song, $downloadPath, $downloadQuality, $io, $output);
         } catch (GetPlaylistJsonException $e) {
             $io->error($e->getMessage());
         }
@@ -134,53 +132,5 @@ class SongCommand extends Command
             $io->text('<info>Lyrics</info>');
             $io->listing([$song->lyricsUrl]);
         }
-    }
-
-    protected function download(Song $song, $path, $quality, OutputStyle $io, OutputInterface $output)
-    {
-        $quality = strtoupper($quality);
-        $filePath = $path . '/' . $song->title . ' - ' . $song->artist;
-        switch ($quality) {
-            case Song::LOSSLESS_QUALITY:
-                if (isset($song->audioUrls[Song::LOSSLESS_QUALITY][0])) {
-                    $url = $song->audioUrls[Song::LOSSLESS_QUALITY][0];
-                    break;
-                }
-            case Song::HIGH_QUALITY:
-                if (isset($song->audioUrls[Song::HIGH_QUALITY][0])) {
-                    $url = $song->audioUrls[Song::HIGH_QUALITY][0];
-                    break;
-                }
-            case Song::LOW_QUALITY:
-                if (isset($song->audioUrls[Song::LOW_QUALITY][0])) {
-                    $url = $song->audioUrls[Song::LOW_QUALITY][0];
-                    break;
-                }
-                throw new DownloadSongException('No audio files available');
-            default:
-                throw new DownloadSongException('Unknown audio quality options');
-        }
-        $matches = [];
-        preg_match('/.*(?<ext>\..*?)(?=\?)/', $url, $matches);
-        $filePath .= $matches['ext'];
-        $client = new Client();
-        $before = true;
-        $progressBar = null;
-        $response = $client->get($url, [
-            'progress' => function ($totalSize, $currentSize) use ($output, &$before, &$progressBar, $song) {
-                if ($totalSize > 0 && $before) {
-                    $progressBar = new ProgressBar($output, $totalSize);
-                    $progressBar->setMessage('<info>' . $song->title . ' - ' . $song->artist . '</info>', 'filename');
-                    $progressBar->setFormat(' %filename% [%bar%] %percent:3s%%');
-                    $before = false;
-                }
-                if (!$before) {
-                    $progressBar->setProgress($currentSize);
-                }
-            }
-        ]);
-        file_put_contents($filePath, $response->getBody());
-        $progressBar->finish();
-        $io->newLine();
     }
 }
---
+++ src/Helper/Helper.php
@@ -0,0 +1,60 @@
+<?php
+namespace Xiami\Console\Helper;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Style\OutputStyle;
+use Symfony\Component\Console\Output\OutputInterface;
+use Xiami\Console\Exception\DownloadSongException;
+use Xiami\Console\Model\Song;
+
+class Helper
+{
+    public static function download(Song $song, $path, $quality, OutputStyle $io, OutputInterface $output)
+    {
+        $quality = strtoupper($quality);
+        $filePath = $path . '/' . $song->title . ' - ' . $song->artist;
+        switch ($quality) {
+            case Song::LOSSLESS_QUALITY:
+                if (isset($song->audioUrls[Song::LOSSLESS_QUALITY][0])) {
+                    $url = $song->audioUrls[Song::LOSSLESS_QUALITY][0];
+                    break;
+                }
+            case Song::HIGH_QUALITY:
+                if (isset($song->audioUrls[Song::HIGH_QUALITY][0])) {
+                    $url = $song->audioUrls[Song::HIGH_QUALITY][0];
+                    break;
+                }
+            case Song::LOW_QUALITY:
+                if (isset($song->audioUrls[Song::LOW_QUALITY][0])) {
+                    $url = $song->audioUrls[Song::LOW_QUALITY][0];
+                    break;
+                }
+                throw new DownloadSongException('No audio files available');
+            default:
+                throw new DownloadSongException('Unknown audio quality options');
+        }
+        $matches = [];
+        preg_match('/.*(?<ext>\..*?)(?=\?)/', $url, $matches);
+        $filePath .= $matches['ext'];
+        $client = new Client();
+        $before = true;
+        $progressBar = null;
+        $response = $client->get($url, [
+            'progress' => function ($totalSize, $currentSize) use ($output, &$before, &$progressBar, $song) {
+                if ($totalSize > 0 && $before) {
+                    $progressBar = new ProgressBar($output, $totalSize);
+                    $progressBar->setMessage('<info>' . $song->title . ' - ' . $song->artist . '</info>', 'filename');
+                    $progressBar->setFormat(' %filename% [%bar%] %percent:3s%%');
+                    $before = false;
+                }
+                if (!$before) {
+                    $progressBar->setProgress($currentSize);
+                }
+            }
+        ]);
+        file_put_contents($filePath, $response->getBody());
+        $progressBar->finish();
+        $io->newLine();
+    }
+}
